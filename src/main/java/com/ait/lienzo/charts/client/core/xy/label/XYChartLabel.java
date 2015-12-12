1a5cc1ff79b8f8890f5fb6a5bac9ac6c1c1db963
Maurycy-Sokolowski <msok@hushmail.com>
2015-12-12 15:49:41 -0800

    Changes for charts

--- src/main/java/com/ait/lienzo/charts/client/core/xy/label/XYChartLabel.java
+++ src/main/java/com/ait/lienzo/charts/client/core/xy/label/XYChartLabel.java
@@ -27,78 +27,72 @@ import com.ait.lienzo.shared.core.types.IColor;
 import com.ait.lienzo.shared.core.types.TextAlign;
 import com.ait.lienzo.shared.core.types.TextBaseLine;
 
-public class XYChartLabel extends Group
-{
-    private static final String FONT_FAMILY = "Verdana";
+public class XYChartLabel extends Group {
+	private static final String FONT_FAMILY = "Verdana";
 
-    private static final String FONT_STYLE  = "";
+	private static final String FONT_STYLE = "";
 
-    private static final int    FONT_SIZE   = 6;
+	private static final int FONT_SIZE = 12;
 
-    private static final IColor LABEL_COLOR = ColorName.BLACK;
+	private static final IColor LABEL_COLOR = ColorName.BLACK;
 
-    private AxisLabel           axisLabel;
+	private AxisLabel axisLabel;
 
-    private Text                label;
+	private Text label;
 
-    private Rectangle           labelContainer;
+	private Rectangle labelContainer;
 
-    public XYChartLabel(AxisLabel axisLabel)
-    {
-        this.axisLabel = axisLabel;
-        build();
-    }
+	public XYChartLabel(AxisLabel axisLabel) {
+		this.axisLabel = axisLabel;
+		build();
+	}
 
-    private void build()
-    {
-        label = new Text(axisLabel.getText(), FONT_FAMILY, FONT_STYLE, FONT_SIZE).setFillColor(LABEL_COLOR).setTextAlign(TextAlign.LEFT).setTextBaseLine(TextBaseLine.TOP);
-        label.setID("label" + axisLabel.getIndex());
-        labelContainer = new Rectangle(1, 1);
-        add(label);
-        add(labelContainer);
-        labelContainer.setAlpha(0.01);
-        labelContainer.moveToTop();
-    }
+	private void build() {
+		label = new Text(axisLabel.getText(), FONT_FAMILY, FONT_STYLE, FONT_SIZE).setFillColor(LABEL_COLOR).setTextAlign(TextAlign.LEFT)
+				.setTextBaseLine(TextBaseLine.TOP);
+		label.setID("label" + axisLabel.getIndex());
+		labelContainer = new Rectangle(1, 1);
+		add(label);
+		add(labelContainer);
+		labelContainer.setAlpha(0.01);
+		labelContainer.moveToTop();
+	}
 
-    public void set(final AxisLabel axisLabel)
-    {
-        this.axisLabel = axisLabel;
-        String text = axisLabel.getText();
-        label.setText(text);
-    }
+	public void set(final AxisLabel axisLabel) {
+		this.axisLabel = axisLabel;
+		String text = axisLabel.getText();
+		label.setText(text);
+	}
 
-    public AxisLabel getAxisLabel()
-    {
-        return axisLabel;
-    }
+	public AxisLabel getAxisLabel() {
+		return axisLabel;
+	}
 
-    public double getLabelWidth()
-    {
-        return label.getBoundingBox().getWidth();
-    }
+	public double getLabelWidth() {
+		return label.getBoundingBox().getWidth();
+	}
 
-    public double getLabelHeight()
-    {
-        return label.getBoundingBox().getHeight();
-    }
+	public double getLabelHeight() {
+		return label.getBoundingBox().getHeight();
+	}
 
-    public String getId()
-    {
-        return label.getID();
+	public String getId() {
+		return label.getID();
 
-    }
+	}
 
-    /* **************************************************************************** */
-    /* Package protected access modifier for methods that expose child group nodes. */
-    /* **************************************************************************** */
+	/* **************************************************************************** */
+	/*
+	 * Package protected access modifier for methods that expose child group
+	 * nodes.
+	 */
+	/* **************************************************************************** */
 
-    Text getLabel()
-    {
-        return label;
-    }
+	Text getLabel() {
+		return label;
+	}
 
-    Rectangle getLabelContainer()
-    {
-        return labelContainer;
-    }
+	Rectangle getLabelContainer() {
+		return labelContainer;
+	}
 }
